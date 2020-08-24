dc36dc5c3b77658d3c85a2ff87bfc5cd3b2b73f3
Cate Dunn <60521577+CateDunn@users.noreply.github.com>
2020-08-24 14:41:11 -0400

    adding useEffect and initial axios call

--- src/App.js
+++ src/App.js
@@ -1,8 +1,21 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
-import Header from './components/Header'
+import Header from './components/Header';
+import axios from 'axios'
 
 const App = () => {
+const [characters, setCharacters] = useState([]);
+const [isLoading, setIsLoading] = useState(true);
+
+useEffect(() => {
+  const fetchCharacters = async () => {
+      const result = await axios(`https://rickandmortyapi.com/api/character/`)
+      console.log(result.data.results)
+  }
+
+  fetchCharacters()
+}, [])
+
   return (
     <div className="container">
       <Header/>
