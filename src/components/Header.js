ba62cbbc97cdf3627f074a84f388024948d33cce
Cate Dunn <60521577+CateDunn@users.noreply.github.com>
2020-08-24 14:21:34 -0400

    logo centered

---
+++ src/components/Header.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import logo from '../Images/logo.png'
+
+const Header = () => {
+    return (
+        <header>
+            <img src={logo} alt=''/>
+        </header>
+    )
+}
+
+export default Header
+
+
+
